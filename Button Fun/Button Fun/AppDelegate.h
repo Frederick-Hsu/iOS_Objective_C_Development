71f253da55be5947d6c41f45d973d0c51ef785cd
徐贊 (Frédérick Hsu) <frederick.hsu@icloud.com>
2015-05-03 22:00:46 +0800

    Append the header guard for the 2 header files : AppDelegate.h    viewController.h

--- Button Fun/Button Fun/AppDelegate.h
+++ Button Fun/Button Fun/AppDelegate.h
@@ -7,13 +7,20 @@
  *
  *************************************************************************************************************************/
 
-#import <UIKit/UIKit.h>
+
+#ifndef APPDELEGATE_H
+#define APPDELEGATE_H
 
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-    @property (strong, nonatomic) UIWindow *window;
+    #import <UIKit/UIKit.h>
 
 
-@end
+    @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+        @property (strong, nonatomic) UIWindow *window;
+
+
+    @end
+
+
+#endif  /*  APPDELEGATE_H  */
