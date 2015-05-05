6c8ccc997543e0c848d9fd6b57bbfe30e2b63c94
徐贊 (Frédérick Hsu) <frederick.hsu@icloud.com>
2015-05-05 23:40:33 +0800

    添加Xcode為該iPhone App.自動生成的框架代碼， 調整好編碼格式後，添加所有的.m 和 .h 程序文件。

---
+++ Control_Fun/Control_Fun/main.m
@@ -0,0 +1,26 @@
+/***************************************************************************************************************************
+ * Project name     : Control_Fun
+ * Description      : 學習研究，開發一款有豐富的圖形用戶界面的iPhone App. :  Control Fun (控件之樂趣)
+ * =========================================================================================================================
+ * File name        : main.m
+ * Description      : This file implements the main() entry function for current project.
+ * Creator          : Frédérick Hsu
+ * Creation date    : Tue.  May 05, 2015
+ * Copyright(C)     2015    All rights reserved.
+ *
+ ***************************************************************************************************************************/
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc,
+                                 argv,
+                                 nil,
+                                 NSStringFromClass([AppDelegate class]));
+    }
+}
