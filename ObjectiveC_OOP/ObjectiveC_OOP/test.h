041fee9eb181d6f09cd4eb5dbfed5258b43aac21
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-13 19:02:20 +0800

    编写测试用例，来测试与验证类，对象，方法的使用。

---
+++ ObjectiveC_OOP/ObjectiveC_OOP/test.h
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ * File name    : test.h
+ * Description  : Declare, implement and perform the project test cases here.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  13 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#ifndef TEST_H
+#define TEST_H
+
+    void performProjectTestCases(void);
+
+    void testHowToUseClassObject(void);
+
+#endif  /* TEST_H */
