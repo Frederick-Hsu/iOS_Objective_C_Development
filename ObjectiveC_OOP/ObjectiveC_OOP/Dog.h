ef1e56b1db04e33a85e27b3a53eb8e54bf59d29b
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-13 21:14:35 +0800

    创建一个子类 ： Dog, 继承自基类 Animal, 并实现其方法函数 -(void)print;

---
+++ ObjectiveC_OOP/ObjectiveC_OOP/Dog.h
@@ -0,0 +1,25 @@
+/**************************************************************************************************
+ * File name    : Dog.h
+ * Description  : Declare the class of Dog.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  13 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#ifndef DOG_H
+#define DOG_H
+
+    #import "Animal.h"
+
+    @interface Dog : Animal
+    {
+        
+    }
+
+        -(void)print;
+
+    @end
+
+#endif  /* DOG_H */
---
+++ ObjectiveC_OOP/ObjectiveC_OOP/Dog.m
@@ -0,0 +1,20 @@
+/**************************************************************************************************
+ * File name    : Dog.m
+ * Description  : Implement the class of Dog.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  13 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "Dog.h"
+
+@implementation Dog
+
+    -(void)print
+    {
+        NSLog(@"This is a dog.");
+        NSLog(@"I am %d years old now.", age);
+    }
+
+@end
