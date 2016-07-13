616af66298141610c1943b37514cd41299766e4d
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-13 21:17:26 +0800

    拟在此测试面向对象编程OOP的3个重要特性 ： Encapsulation(封装)，Inheritance(继承)，Polymorphism(多态)。

---
+++ ObjectiveC_OOP/ObjectiveC_OOP/test_Encapsulation_Inheritance_Polymorphism.h
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * File name    : test_Encapsulation_Inheritance_Polymorphism.h
+ * Description  : Test and verify the 3 important features of OOP
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  13 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#ifndef TEST_ENCAPSULATION_INHERITANCE_POLYMORPHISM_H
+#define TEST_ENCAPSULATION_INHERITANCE_POLYMORPHISM_H
+
+
+#endif /* TEST_ENCAPSULATION_INHERITANCE_POLYMORPHISM_H */
---
+++ ObjectiveC_OOP/ObjectiveC_OOP/test_Encapsulation_Inheritance_Polymorphism.m
@@ -0,0 +1,11 @@
+/**************************************************************************************************
+ * File name    : test_Encapsulation_Inheritance_Polymorphism.m
+ * Description  : Test and verify the 3 important features of OOP
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  13 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#import "test_Encapsulation_Inheritance_Polymorphism.h"
