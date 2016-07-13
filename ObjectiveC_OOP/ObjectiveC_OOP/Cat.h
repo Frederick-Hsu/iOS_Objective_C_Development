9af63b2ea3a1f7891e34b6f66e296f387215ac00
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-13 21:15:44 +0800

    创建一个子类 ： Cat, 继承自基类 Animal, 并实现其方法函数 -(void)print;

---
+++ ObjectiveC_OOP/ObjectiveC_OOP/Cat.h
@@ -0,0 +1,25 @@
+/**************************************************************************************************
+ * File name    : Cat.h
+ * Description  : Declare the class of Cat.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  13 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#ifndef CAT_H
+#define CAT_H
+
+    #import "Animal.h"
+
+    @interface Cat : Animal
+    {
+        
+    }
+
+        -(void)print;
+
+    @end
+
+#endif  /* CAT_H */
