f49947af0aedda43ffdb310085401e26609e3007
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-24 17:20:47 +0800

    创建 ObjectiveC_AdvancedFeatures 项目， 学习研究Objective-C编程的一些高级特性。

---
+++ ObjectiveC_AdvancedFeatures/ObjectiveC_AdvancedFeatures/main.m
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+ * Project name : ObjectiveC_AdvancedFeatures
+ * Description  : Learn and study some advanced or special features of Objective-C, as an OOP language.
+ * =================================================================================================
+ * File nae     : main.m
+ * Description  : This file implements the main() portal function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Sun.  24 JULY, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool
+    {
+    }
+}
