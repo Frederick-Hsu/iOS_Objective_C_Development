d07a063f3192ce84a6310ab5a9e0209f93e4a2a2
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-24 16:37:46 +0800

    定义与实现类 CStudent   使用@property  和 @synthesize 来让编译器决定自动生成什么样的 Setter 和 Getter 成员变量，来代替私有成员变量的访问和存储。
    @property (nonatomic, assign) int age;
    @synthesize age = _age;

---
+++ ObjectiveC_OOP/ObjectiveC_OOP/OOP/CStudent.h
@@ -0,0 +1,22 @@
+/**************************************************************************************************
+ * File name    : Student.h
+ * Description  : Implement the class of Student.
+ * Creator      : Frederick Hsu
+ * Creation date: Fri.  22 July, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#ifndef CSTUDENT_H
+#define CSTUDENT_H
+
+    #import <Foundation/Foundation.h>
+
+    @interface CStudent : NSObject
+
+        @property (nonatomic, assign) int age;
+
+    @end
+
+#endif  /* CSTUDENT_H */
