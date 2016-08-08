bf422c042fdcc9d812c8ab8cc9a9d61164313ff7
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-08-09 00:51:35 +0800

    基类Person,  带有一个成员函数 -(void)run;

---
+++ ObjectiveC_AdvancedFeatures/ObjectiveC_AdvancedFeatures/Category/Person.h
@@ -0,0 +1,22 @@
+/**************************************************************************************************
+ * File nae     : Person.h
+ * Description  : Declare the class Person.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  04 Aug., 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#ifndef PERSON_H
+#define PERSON_H
+
+    #import <Foundation/Foundation.h>
+
+    @interface Person : NSObject
+
+        -(void)run;
+
+    @end
+
+#endif  /* PERSON_H */
---
+++ ObjectiveC_AdvancedFeatures/ObjectiveC_AdvancedFeatures/Category/Person.m
@@ -0,0 +1,20 @@
+/**************************************************************************************************
+ * File nae     : Person.m
+ * Description  : Implement the class Person.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  04 Aug., 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#import "Person.h"
+
+@implementation Person
+
+    -(void)run
+    {
+        NSLog(@"I am running...");
+    }
+
+@end
