4201c064683d86c17fe182a51a367d42ea13534e
Frederick Hsu <frederick.xu@amphenolmcp.com>
2016-12-27 19:58:20 +0800

    类Secretary实现protocol  <TicketDelegate>里的2个方法 -(int)leftTicketNumber         -(double)ticketPrice

---
+++ ObjectiveC_AdvancedFeatures/ObjectiveC_AdvancedFeatures/Protocol_and_Proxy/Secretary.m
@@ -0,0 +1,25 @@
+/**************************************************************************************************
+ * File name    : Secretary.c
+ * Description  : Implement the class of Secretary.
+ * Creator      : Frederick Hsu
+ * Creation date: Tue.  27 Dec. 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#import "Secretary.h"
+
+@implementation Secretary
+
+    -(int)leftTicketNumber
+    {
+        return 10;
+    }
+
+    -(double)ticketPrice
+    {
+        return 500.00;
+    }
+
+@end
