63f4c50776071740912482bddff293aeb587be78
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-01-12 19:32:38 +0800

    设计测试用例验证NSString字符串对象的操作。

---
+++ Foundation_Framework/Foundation_Framework/String/testString.m
@@ -0,0 +1,28 @@
+/**************************************************************************************************
+ * File name    : testString.m
+ * Description  : Design test cases to verify the String module.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  28 Dec. 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "testString.h"
+
+#import "String.h"
+#import <Foundation/Foundation.h>
+
+void testCase4_String(void)
+{
+    NSLog(@"Verify the NSString initialization : ");
+    initializeString();
+    
+    NSLog(@"Verify the transferring between C type of string and Objective-C type of string : ");
+    transferStringBetween_C_and_ObjectiveC();
+    
+    NSLog(@"List some basic operations for string :");
+    stringBasicOps();
+    
+    NSLog(@"Judge and compare 2 strings : ");
+    judgeAndCompareString();
+}
---
+++ Foundation_Framework/Foundation_Framework/test.h
@@ -0,0 +1,17 @@
+/**************************************************************************************************
+ * File name    : test.h
+ * Description  : Perform the test cases here.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  28 Dec. 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#ifndef TEST_H
+#define TEST_H
+
+    void performTestCases(void);
+
+    void performTestCases4StringModule(void);
+
+#endif  /* TEST_H */
