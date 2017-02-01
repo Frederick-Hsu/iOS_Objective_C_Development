56f109974f30eaf650927b36de24eac66fbb91e2
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-01 20:16:41 +0800

    Add 4 code files : Dictionary.m  Dictionary.h  testDictionary.m  testDictionary.h into the project under Dictionary/ directory.

---
+++ Foundation_Framework/Foundation_Framework/Dictionary/Dictionary.h
@@ -0,0 +1,14 @@
+/**************************************************************************************************
+ * File name    : Dictionary.h
+ * Description  : Study the NSDictionary and NSMutableDictionary key-value type.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  01 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+
+#endif  /* DICTIONARY_H */
