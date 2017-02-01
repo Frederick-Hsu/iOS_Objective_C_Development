0f08df06f9b1b75c95f608b168dded3186a5bb2f
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-01 16:36:21 +0800

    Design test cases to verify the NSArray objects array.

---
+++ Foundation_Framework/Foundation_Framework/Array/testArray.m
@@ -0,0 +1,19 @@
+/**************************************************************************************************
+ * File name    : testArray.m
+ * Description  : Design test cases to perform the testing for NSArray objects' operations.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  01 Feb. 2017
+ * COpyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "testArray.h"
+#import "Array.h"
+
+#import <Foundation/Foundation.h>
+
+void testCase4_Array(void)
+{
+    NSLog(@"Verify how to create the the array consisted of NSArray objects.");
+    createArray();
+}
