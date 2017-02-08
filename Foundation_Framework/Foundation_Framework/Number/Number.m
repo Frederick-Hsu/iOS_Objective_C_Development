e65527ce7b7b0a446c12fec86cb46e0c3ed808f1
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-08 20:27:10 +0800

    学习研究NSNumber型的数字对象，及其如何操作这些对象。

---
+++ Foundation_Framework/Foundation_Framework/Number/Number.m
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * File name    : Number.m
+ * Description  : Study the NSNumber class of objects and its operations.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  08 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "Number.h"
+
+void initAndCreateNumberObj(void)
+{
+    
+}
