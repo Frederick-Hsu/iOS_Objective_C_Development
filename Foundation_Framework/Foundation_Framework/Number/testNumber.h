ba70e6da127d06dd4d65833d12fe11c2bc08defa
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-08 20:29:04 +0800

    Write the test cases to perform the testing for Number module.

---
+++ Foundation_Framework/Foundation_Framework/Number/testNumber.h
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * File name    : testNumber.h
+ * Description  : Design the test cases to perform testing for NSNumber objects.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  08 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#ifndef TEST_NUMBER_H
+#define TEST_NUMBER_H
+
+    void testCase4_Number(void);
+
+#endif  /* TEST_NUMBER_H */
---
+++ Foundation_Framework/Foundation_Framework/Number/testNumber.m
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * File name    : testNumber.m
+ * Description  : Design the test cases to perform testing for NSNumber objects.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  08 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "testNumber.h"
+#import "Number.h"
+
+void testCase4_Number(void)
+{
+}
