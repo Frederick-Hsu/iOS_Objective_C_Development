0372788909e696585bcc0ee5cadbf314bd743826
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-11 18:08:20 +0800

    Declare the prototype of void readPropertyListFileWithMutableDict(void);   try to read the plist file.

---
+++ ReadWriteFile/ReadWriteFile/read_plist.h
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * File name    : read_plist.h
+ * Description  : study how to read the property list file which is stored in XML format.
+ * Creator      : Frederick Hsu
+ * Creation date: Sat.  11 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#ifndef READ_PLIST_H
+#define READ_PLIST_H
+
+    void readPropertyListFileWithMutableDict(void);
+
+#endif  /* READ_PLIST_H */
---
+++ ReadWriteFile/ReadWriteFile/read_plist.m
@@ -0,0 +1,14 @@
+/**************************************************************************************************
+ * File name    : read_plist.m
+ * Description  : study how to read the property list file which is stored in XML format.
+ * Creator      : Frederick Hsu
+ * Creation date: Sat.  11 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "read_plist.h"
+
+void readPropertyListFileWithMutableDict(void)
+{
+}
