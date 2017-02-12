372c319e5e0faac873cd6ff6eaf408971b0a07ec
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-12 19:20:47 +0800

    测试验证创建文件，拷贝文件，移动和删除文件的测试用例。

--- ReadWriteFile/ReadWriteFile/NSFileManager/test_file_directory_management.m
+++ ReadWriteFile/ReadWriteFile/NSFileManager/test_file_directory_management.m
@@ -18,4 +18,20 @@ void testCase4_FileDirManagement(void)
     NSLog(@"\n\n");
     NSLog(@"Verify the management for file and directory : ");
     createAndRetrievePath();
+    
+    NSLog(@"\n\n");
+    NSLog(@"Create the user-specified file at target directory : ");
+    createFilePath();
+    
+    NSLog(@"\n\n");
+    NSLog(@"Copy your specified file :");
+    copyFileAtPath();
+    
+    NSLog(@"\n\n");
+    NSLog(@"At last, removed the user-specified file : ");
+    deleteFileAtPath();
+    
+    NSLog(@"\n\n");
+    NSLog(@"Move your file to the destination folder : ");
+    moveFileAtPath();
 }
