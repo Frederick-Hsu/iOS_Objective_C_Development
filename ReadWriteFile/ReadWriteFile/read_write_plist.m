a93a1324b365b53403dfca32b37667cb4eceda13
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-12 15:16:49 +0800

    Rename the old file "read_plist.h" and "read_plist.m" to "read_write_plist.h" and "read_write_plist.m" respectively.

---
+++ ReadWriteFile/ReadWriteFile/read_write_plist.m
@@ -0,0 +1,19 @@
+/**************************************************************************************************
+ * File name    : read_write_plist.m
+ * Description  : study how to read the property list file which is stored in XML format.
+ * Creator      : Frederick Hsu
+ * Creation date: Sat.  11 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import "read_write_plist.h"
+#import <Foundation/Foundation.h>
+
+void readPropertyListFileWithMutableDict(void)
+{
+    NSMutableDictionary *dict_plist = [NSMutableDictionary dictionaryWithContentsOfFile:
+                                       @"/Users/frederick_hsu/Programming/Objective_C_Programming/ReadWriteFile/ReadWriteFile/Student.plist"];
+    
+    NSLog(@"Display the elements of plist file : %@", dict_plist);
+}
