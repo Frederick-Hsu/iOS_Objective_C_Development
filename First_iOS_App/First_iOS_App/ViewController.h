30cef8d7f2bb117c5a7fc8c389a007e64a0685f8
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-02-17 22:54:35 +0800

    Add the definition and implementation codes of ViewController class.
    Just customize and beautify the code format and indentation style.

---
+++ First_iOS_App/First_iOS_App/ViewController.h
@@ -0,0 +1,17 @@
+/**************************************************************************************************
+ * File name    : ViewController.h
+ * Description  : Define the prototype of View Controller.
+ * Creator      : Frederick Hsu
+ * Creation date: Fri.  17 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ First_iOS_App/First_iOS_App/ViewController.m
@@ -0,0 +1,33 @@
+/**************************************************************************************************
+ * File name    : ViewController.m
+ * Description  : Implement the View Controller.
+ * Creator      : Frederick Hsu
+ * Creation date: Fri.  17 Feb. 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+    - (void)viewDidLoad
+    {
+        [super viewDidLoad];
+        /* Do any additional setup after loading the view, typically from a nib. */
+    }
+
+
+    - (void)didReceiveMemoryWarning
+    {
+        [super didReceiveMemoryWarning];
+        /* Dispose of any resources that can be recreated. */
+    }
+
+
+@end
