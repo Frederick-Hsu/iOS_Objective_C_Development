fda877bd84e05cf1eb4bc7009b411cb92f7d8226
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2017-02-23 01:29:51 +0800

    Start my first iOS application "Hello_World" development.
    It is a virginal starter.

---
+++ Hello_World/Hello_World/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hello_World
+//
+//  Created by 徐贊 (Frédérick Hsu) on 2/22/17.
+//  Copyright © 2017 Frederick Hsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
