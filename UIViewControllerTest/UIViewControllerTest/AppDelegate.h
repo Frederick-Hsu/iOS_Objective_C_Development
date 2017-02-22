4ea8d2a1fddd10b370a643f00889c0fd925b9920
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2017-02-23 01:31:18 +0800

    Create a new project "UIViewControllerTest".

---
+++ UIViewControllerTest/UIViewControllerTest/AppDelegate.h
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ * File name    : AppDelegate.h
+ * Description  : Define the AppDelegate class.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  23 Feb. 2017
+ * Copyright(C  2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
