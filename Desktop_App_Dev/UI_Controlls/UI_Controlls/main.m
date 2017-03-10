241a9210b1220f5f25b48cfdf41612d95aaf1e2b
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-03-10 11:10:52 +0800

    Update the main.m source file and the project setting file : project.pbxproj

--- Desktop_App_Dev/UI_Controlls/UI_Controlls/main.m
+++ Desktop_App_Dev/UI_Controlls/UI_Controlls/main.m
@@ -1,13 +1,18 @@
-//
-//  main.m
-//  UI_Controlls
-//
-//  Created by Frederick Hsu on 09/03/2017.
-//  Copyright © 2017 Frederick Hsu. All rights reserved.
-//
+/**************************************************************************************************
+ * Project name : UI_Controlls
+ * Description  : Study the UI basic controlls of Mac OS X desktop Cocoa application.
+ * ================================================================================================
+ * File name    : mian.m
+ * Description  : This file implements the main() portal function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  09 March 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     return NSApplicationMain(argc, argv);
 }
