24571f9fe3cc4d6e9d721c2d0baf0c2d7768c3de
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-03-10 11:34:09 +0800

    1. Add 5 IBOutlet properties and 1 IBAction method.
    2. Implement the NSTextField event/action callback functions :
    	- (void)controlTextDidBeginEditing:(NSNotification *)obj
    	- (void)controlTextDidEndEditing:(NSNotification *)obj
    	- (void)controlTextDidChange:(NSNotification *)obj
    
    3. Implement an IBAction method : - (IBAction)searchAction:(id)sender   associated with the NSSearchField UI control
       meanwhile register (- (void)registerSearchButtonAction) 2 button cells inside the NSSearchField control, assign them targets and actions :
    	- (IBAction)searchButtonClicked:(id)sender
    	- (IBAction)cancelButtonClicked:(id)sender

--- Desktop_App_Dev/UI_Controlls/UI_Controlls/AppDelegate.m
+++ Desktop_App_Dev/UI_Controlls/UI_Controlls/AppDelegate.m
@@ -1,28 +1,124 @@
-//
-//  AppDelegate.m
-//  UI_Controlls
-//
-//  Created by Frederick Hsu on 09/03/2017.
-//  Copyright © 2017 Frederick Hsu. All rights reserved.
-//
+/**************************************************************************************************
+ * File name    : AppDelegate.m
+ * Description  : Implement the AppDelegate class, implement the main UI actions.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  09 March 2017
+ * Copyright(C) 2017    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
 
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+    @property (weak) IBOutlet NSWindow *loginWindow;
+    @property (weak) IBOutlet NSWindow *editorWindow;
+
+
+    @property (weak) IBOutlet NSTextField *usernameTextField;
+    @property (weak) IBOutlet NSSecureTextField *passwordSecureTextField;
+
+    @property (weak) IBOutlet NSSearchField *searchField;
+
+    - (IBAction)searchAction:(id)sender;
+
+
 @end
+
+/*================================================================================================*/
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
+    - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+    {
+        /* Insert code here to initialize your application */
+        [self registerSearchButtonAction];
+    }
 
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
+    - (void)applicationWillTerminate:(NSNotification *)aNotification
+    {
+        /* Insert code here to tear down your application */
+    }
 
+/*================================================================================================*/
+    #pragma mark-- NSTextField
 
+    - (void)controlTextDidBeginEditing:(NSNotification *)obj    /* 光标进入输入框，第一次输入得到事件通知 */
+    {
+        id text = obj.object;
+        if (text == self.usernameTextField)
+        {
+            NSLog(@"controlTextDidBeginEditing usernameTextField %@", self.usernameTextField.stringValue);
+        }
+        if (text == self.passwordSecureTextField)
+        {
+            NSLog(@"controlTextDidBeginEditing passwordSecureTextField %@", self.passwordSecureTextField.stringValue);
+        }
+    }
+
+    - (void)controlTextDidEndEditing:(NSNotification *)obj      /* 光标离开输入框时得到事件通知 */
+    {
+        id text = obj.object;
+        if (text == self.usernameTextField)
+        {
+            NSLog(@"controlTextDidEndEditing usernameTextField %@", self.usernameTextField.stringValue);
+        }
+        else if (text == self.passwordSecureTextField)
+        {
+            NSLog(@"controlTextDidEndEditing passwordSecureTextField %@", self.passwordSecureTextField.stringValue);
+        }
+    }
+
+    - (void)controlTextDidChange:(NSNotification *)obj          /* 文本框正在输入，内容变化时得到事件通知 */
+    {
+        id text = obj.object;
+        if (text == self.usernameTextField)
+        {
+            NSLog(@"controlTextDidChange usernameTextField %@", self.usernameTextField.stringValue);
+        }
+        else if (text == self.passwordSecureTextField)
+        {
+            NSLog(@"controlTextDidChange passwordSecureTextField %@", self.passwordSecureTextField.stringValue);
+        }
+    }
+/*================================================================================================*/
+
+    #pragma mark-- NSSearchField
+
+    - (IBAction)searchAction:(id)sender
+    {
+        NSSearchField *searchField = sender;
+        NSString *content = searchField.stringValue;
+        NSLog(@"content : %@", content);
+    }
+
+    - (void)registerSearchButtonAction
+    {
+        NSActionCell *searchButtonCell = [[self.searchField cell] searchButtonCell];
+        NSActionCell *cancelButtonCell = [[self.searchField cell] cancelButtonCell];
+        searchButtonCell.target = self;
+        searchButtonCell.action = @selector(searchButtonClicked:);
+        cancelButtonCell.target = self;
+        cancelButtonCell.action = @selector(cancelButtonClicked:);
+    }
+
+    - (IBAction)searchButtonClicked:(id)sender
+    {
+        NSSearchField *searchFieldSender = sender;
+        NSString *content = searchFieldSender.stringValue;
+        NSLog(@"searchButtonClicked : %@", content);
+    }
+
+    - (IBAction)cancelButtonClicked:(id)sender
+    {
+        NSSearchField *searchFieldSender = sender;
+        searchFieldSender.stringValue = @"";    // Clean the serachField text.
+        
+        NSLog(@"cancelButtonClicked");
+    }
+
+/*================================================================================================*/
 @end
