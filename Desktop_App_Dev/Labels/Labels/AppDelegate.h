f73508e5703691933f4d7ac9e3a8313d30dc2897
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-03-10 15:34:49 +0800

    学习研究在UI窗口上设计Label， 编程控制Label.

---
+++ Desktop_App_Dev/Labels/Labels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labels
+//
+//  Created by Frederick Hsu on 10/03/2017.
+//  Copyright © 2017 Frederick Hsu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+    - (void)addLabel;
+    - (void)addRichTextLabel;
+
+@end
+
---
+++ Desktop_App_Dev/Labels/Labels/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Labels
+//
+//  Created by Frederick Hsu on 10/03/2017.
+//  Copyright © 2017 Frederick Hsu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
