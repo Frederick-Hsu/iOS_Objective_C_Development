798e62d619d17a7fe218894fc560c392a1821feb
Frederique Hsu <frederique.hsu@outlook.com>
2020-03-27 02:55:10 +0800

    New code files were added into this "manip_objs" project, update the Makefile and main.m

--- manip_objs/main.m
+++ manip_objs/main.m
@@ -14,6 +14,7 @@
 
 #import <Foundation/Foundation.h>
 #import "Circle.h"
+#import "testing.h"
 
 int main(int argc, char* argv[])
 {
@@ -23,7 +24,17 @@ int main(int argc, char* argv[])
     [circle setR:3 andX:4 andY:5];
     [circle print];
     NSLog(@"sum = %d", [Circle allNum]);
+
+    circle.radius = 100;    /* Call the setter method */
+    // NSLog(@"Circle radius: %d, x: %d, y: %d", [circle radius], [circle x], [circle y]);     /* call the getter method */
+    NSLog(@"Circle radius: %d, x: %d, y:%d", [circle radius], circle.gr/* or [circle gr]*/, [circle y]);
+    [circle setRadius:50];
+    [circle sr:9];
+    NSLog(@"Now the radius had been changed to %d", [circle radius]);
+
     [circle release];
+
+    accessCompositeObjects();
 
     [pool drain];
     return 0;
